455803744aa229651d49c381cdf31345374ffd71
Prashant Yadav <Prashant.Y@Orioninc.com>
2019-01-02 18:19:56 +0530

    Added passport for google authentication

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStratergy = require('passport-google-oauth20');
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+passport.use(
+  new GoogleStratergy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+      console.log(accessToken);
+  })
+);
 
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile','email']
+  })
+);
+  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
