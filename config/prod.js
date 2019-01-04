8e76d773205e26785441f5e0ef384225d01ed1b4
Prashant Yadav <Prashant.Y@Orioninc.com>
2019-01-04 11:56:13 +0530

    Added produnction and development vars

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID ,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+//mongodb://prashant:password12345@ds147734.mlab.com:47734/emaily-dev-prashant
