d36fa6e18eb88291112ff77139db58a19c6376db
Prashant Yadav <Prashant.Y@Orioninc.com>
2019-01-08 17:14:42 +0530

    Update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component{
                 <li key="2" style={{margin:"0 10px"}}>Credits: {this.props.data.auth.credits}</li>,
                 <li key="3">
                     <a href="/api/logout">Logout</a>
-                </li>
+                </li> 
             ];
         }
     }
