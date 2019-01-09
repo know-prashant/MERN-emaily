33804067d0f7d409ac018e5ac05f0924412e4090
Prashant Yadav <Prashant.Y@Orioninc.com>
2019-01-09 11:40:46 +0530

    Finished configuration for heroku deployment

--- index.js
+++ index.js
@@ -29,8 +29,22 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === 'production'){
+  //Express will serve up production assets
+  //like our main.hs file, or main.css file!
+  app.use(express.static('client/build'));
+  
+  //Express will serve up index.html file
+  //If it doesnot recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
